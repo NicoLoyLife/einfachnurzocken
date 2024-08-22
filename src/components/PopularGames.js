7f45039ff9fb5a0fe065d510358f0dc6c72bb2e0
Nico Götze <goetze.nico@hotmail.de>
2024-08-22 22:00:50 +0200

    Mit der Visualisierung der Spiele begonnen

--- src/components/PopularGames.js
+++ src/components/PopularGames.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Grid } from '@mui/material';
 import { spiele } from '../services/dummyData';
+import GameCard from './games/GameCard';
 
 function PopularGames() {
+  // Sortieren der Spiele nach Rating in absteigender Reihenfolge
+  const sortedGames = spiele.sort((a, b) => b.rating - a.rating);
+
+  // Filtern der Spiele mit den höchsten Ratings
+  const topGames = sortedGames.slice(0, 6);
+
   return (
     <Box sx={{ mt: 8, mb: 8 }}>
       <Typography variant='h3' component='h3' sx={{ mb: 4, textAlign: 'center' }}>
@@ -12,6 +19,14 @@ function PopularGames() {
       <Typography variant='body1' component='p' sx={{ mb: 4, textAlign: 'center' }}>
         Entdecke die Spiele, die aktuell bei unseren Nutzern am beliebtesten sind. Diese Slots und Tischspiele bieten nicht nur Unterhaltung, sondern auch großartige Gewinnchancen.
       </Typography>
+
+      <Grid container spacing={1}>
+        {topGames.map((game, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4}>
+            <GameCard game={game} />
+          </Grid>
+        ))}
+      </Grid>
 
       <Button variant='contained' color='primary' sx={{ display: 'block', margin: '0 auto' }} size='large'>
         Alle Spiele ansehen
---
+++ src/components/games/GameCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Typography, Card, CardMedia, CardContent, IconButton } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+function GameCard({ game }) {
+  return (
+    <Card sx={{ borderRadius: '15px', overflow: 'hidden', position: 'relative' }}>
+      <CardMedia
+        component="img"
+        alt={game.name}
+        image={game.image}
+        sx={{ height: '200px', objectFit: 'cover' }}
+      />
+
+      <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'primary.dark', color: '#fff', padding: '10px 16px', minHeight: '60px' }}>
+        <Typography variant='body1'>
+          {game.name}
+        </Typography>
+        <IconButton color="inherit" sx={{ padding: 0 }}>
+          <ArrowForwardIcon />
+        </IconButton>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default GameCard;
