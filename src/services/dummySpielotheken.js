4abd5c5193d1fbd38c92d4625237f3150ce9d795
Nico Götze <n.goetze@timmehosting.de>
2024-09-16 16:09:34 +0200

    Grundstruktur Betano Review

---
+++ src/services/dummySpielotheken.js
@@ -0,0 +1,16 @@
+// Bewertungskriterien Casino
+
+// Bonusangebote & Promotionen: Willkommensbonus, Einzahlungsboni, Freispiele, VIP-Programme
+// 0/5
+
+// Spielauswahl: Vielfalt an Slots, Tischspielen, Live-Casino, Anbieter (z.B. NetEnt, Play’n GO, Pragmatic Play,Klassiker wie Book of Ra, Lord of the Ocean, Faust und Lucky Lady's Charm )
+// 5/5
+
+// Kundensupport: Erreichbarkeit (24/7), Kanäle (Live-Chat, E-Mail, Telefon), Qualität der Hilfe
+// 4/5
+
+// Treueprogramme: VIP-Level, exklusive Belohnungen, Prämien und Boni für Stammkunden
+// 5/5
+
+// Auszahlungsbedingungen: Fairness der Umsatzbedingungen, Auszahlungszeiten, Transparen
+// 5/5
---
+++ src/services/dummySportwetten.js
@@ -0,0 +1,28 @@
+// Bewertungskriterien Sportwetten
+
+// vBonusangebote & Promotionen: Willkommensbonus, Einzahlungsboni, Gratiswetten, Cashback, Treueprogramme
+// 5/5
+
+// Wettquoten: Attraktivität der Quoten im Vergleich zum Markt, insbesondere bei populären Sportarten
+// 4/5
+
+// Sportarten- & Wettangebot: Vielfalt der angebotenen Sportarten und Ligen, Spezialwetten (z.B. Spielerwetten, Handicap-Wetten)
+// 4/5
+
+// Zahlungsmethoden: Auswahl an Zahlungsmethoden (Kreditkarte, PayPal, Banküberweisung), Auszahlungszeiten, Gebühren
+// 5/5
+
+// Kundensupport: Erreichbarkeit (Live-Chat, E-Mail, Telefon), Qualität des Supports, Reaktionszeit
+// 4/5
+
+// Wettsteuer & Gebühren: Behandlung der Wettsteuer (steuerfrei bei bestimmten Wetten), anfallende Gebühren für Ein- und Auszahlungen
+// 4/5
+
+// Mobile App & Optimierung: Funktionalität der mobilen App oder Webseite, reibungslose Nutzung auf verschiedenen Geräten
+// 4/5
+
+// Treueprogramme & VIP-Services: Exklusive Angebote für Stammkunden, VIP-Betreuung, regelmäßige Promotionen
+// 4/5
+
+// Cash-Out-Funktion: Verfügbarkeit und Flexibilität der Cash-Out-Option (automatisch, teilweiser Cash-Out)
+// 3/5
