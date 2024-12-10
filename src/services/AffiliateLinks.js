f6e703d634522fa838d60cd7b78bd124b3268b54
Nico Götze <goetze.nico@hotmail.de>
2024-12-10 19:43:08 +0100

    Neuer Ref-Link Betano

--- src/services/AffiliateLinks.js
+++ src/services/AffiliateLinks.js
@@ -1,5 +1,6 @@
 const AffiliateLinks = {
-    betanoSportwetten: "https://gml-grp.com/C.ashx?btag=a_10846b_3503c_&affid=3453&siteid=10846&adid=3503&c=",
+    // betanoSportwetten: "https://gml-grp.com/C.ashx?btag=a_10846b_3503c_&affid=3453&siteid=10846&adid=3503&c=",
+    betanoSportwetten: "https://gml-grp.com/C.ashx?btag=a_57499b_3706c_&affid=3453&siteid=57499&adid=3706&c=",
     winamaxSportwetten: "https://www.winamax.de/registration/landing/Default?language=DE&banid=73017",
     merkurbetsSportwetten: "https://media.cashpointpartners.com/C.ashx?btag=a_8433b_2360c_&affid=4834&siteid=8433&adid=2360&c=",
     bingBongSlots: "https://opwu.grass.bingbong.de/ts/i5044752/tsc?typ=r&amc=networks.cap.526088.535942.CRTBbxuBVCq",
